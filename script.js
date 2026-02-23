4dfdb7537950c00644a5a2ee1a8f0b79c20ea711
Anikesh Aman <96464888+anikeshaman@users.noreply.github.com>
2026-02-23 23:36:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -167,5 +167,24 @@ function addToHistory(task){
 
   historyList.prepend(div);
 }
+/* DARK MODE TOGGLE */
+const themeToggle = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+  themeToggle.textContent = "☀️";
+}
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    themeToggle.textContent = "☀️";
+  } else {
+    localStorage.setItem("theme", "light");
+    themeToggle.textContent = "🌙";
+  }
+});
 
 
